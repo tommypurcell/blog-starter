f7427172549fb653762934013c270727d46b416b
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2024-01-06 00:54:57 -0800

    added sign in to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,26 +3,25 @@
 import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
 import { Button } from "./ui/button";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
 
 const Navbar = async () => {
-  // const { userId } = auth();
-
-  // if (!userId) {
-  //   redirect("/sign-in");
-  // }
-
-  // const stores = await prismadb.store.findMany({
-  //   where: {
-  //     userId,
-  //   },
-  // });
+  const { userId } = auth();
 
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <MainNav />
         <div className="ml-auto flex items-center space-x-4"></div>
-        <UserButton />
+
+        {userId ? (
+          <UserButton />
+        ) : (
+          <Link href="/sign-in">
+            <Button>Sign in</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
