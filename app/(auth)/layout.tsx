8f4dcd5d023e854a5295e39d3d8a23b1222a5aaf
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2024-01-06 01:07:29 -0800

    sign-in sign-up pages dynamic title

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,23 @@
+"use client";
+import Navbar from "@/components/navbar";
+import { usePathname } from "next/navigation";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  // Determine the text based on the current pathname
+  const text = pathname.includes("/sign-up") ? "Sign Up" : "Sign In";
+
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
+    <>
+      <div className="flex flex-col space-y-4 items-center justify-center h-screen">
+        <h1 className="text-5xl font-bold ">{text}</h1>
+        <div>{children}</div>
+      </div>
+    </>
   );
 }
