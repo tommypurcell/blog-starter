4611f74c817ae7d84afd989546f0fead8cf05d15
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2024-01-07 03:43:04 -0800

    added search bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,27 +1,28 @@
-// import { UserButton, auth } from "@clerk/nextjs";
-
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
 import { Button } from "./ui/button";
-import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { Input } from "@/components/ui/input";
 
 const Navbar = async () => {
   const { userId } = auth();
 
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
+      <div className="flex h-16 items-center justify-between px-4">
         <MainNav />
-        <div className="ml-auto flex items-center space-x-4"></div>
-
-        {userId ? (
-          <UserButton />
-        ) : (
-          <Link href="/sign-in">
-            <Button>Sign in</Button>
-          </Link>
-        )}
+        <div className="flex-1 flex justify-center px-4 max-w-xs">
+          <Input className="w-full" placeholder="Search Posts" />
+        </div>
+        <div className="flex items-center space-x-4">
+          {userId ? (
+            <UserButton />
+          ) : (
+            <Link href="/sign-in">
+              <Button>Sign in</Button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
